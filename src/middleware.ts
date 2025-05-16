8aa4b92cd680553e11e532fdbe83010edd359c62
honeystore365 <honeystore365@gmail.com>
2025-05-16 18:29:03 +0000

    install supabase/ssr

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,22 @@
-import { createServerClient } from '@supabase/auth-helpers-nextjs'
+import { createMiddlewareClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   try {
-    let supabaseResponse = NextResponse.next({
-      request,
+    // Create an unmodified response
+    let response = NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
     })
 
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return request.cookies.getAll()
-          },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              supabaseResponse.cookies.set(name, value, options)
-            );
-          },
-        },
-      }
-    )
+    const supabase = createMiddlewareClient({ req: request, res: response })
 
-    // Do not run code between createServerClient and
-    // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+    // This will refresh session if expired - otherwise just update the cookies
+    await supabase.auth.getSession()
+
+    // Do not run code between createMiddlewareClient and
+    // supabase.auth.getSession(). A simple mistake could make it very hard to debug
     // issues with users being randomly logged out.
 
     // IMPORTANT: DO NOT REMOVE auth.getUser()
