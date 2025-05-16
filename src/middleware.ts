b64b52db885dfc9bffe167d2ea3bc5d81c4b840c
honeystore365 <honeystore365@gmail.com>
2025-05-16 18:55:22 +0000

    second try

--- src/middleware.ts
+++ src/middleware.ts
@@ -104,7 +104,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?:?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)).*)',
+    '/((?:(?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)).)*)',
     '/admin/:path*',
     '/auth/login',
     '/auth/register',
